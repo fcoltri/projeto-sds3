08584ba5edae99878c9fc050da5eb8f1927ca0cf
Fernando Coltri <fcoltri@gmail.com>
2021-09-09 16:14:02 -0300

    Layers

---
+++ backend/src/main/java/br/com/treswxsolutions/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package br.com.treswxsolutions.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.treswxsolutions.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
