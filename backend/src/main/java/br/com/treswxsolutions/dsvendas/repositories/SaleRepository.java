279bac3f7babbdcf691326c3be0a19ccd46a1983
Fernando Coltri <fcoltri@gmail.com>
2021-09-10 14:14:36 -0300

    Group by search

--- backend/src/main/java/br/com/treswxsolutions/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/br/com/treswxsolutions/dsvendas/repositories/SaleRepository.java
@@ -1,11 +1,26 @@
 package br.com.treswxsolutions.dsvendas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import br.com.treswxsolutions.dsvendas.dto.SaleSuccessDTO;
+import br.com.treswxsolutions.dsvendas.dto.SaleSumDTO;
 import br.com.treswxsolutions.dsvendas.entities.Sale;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+	@Query("SELECT new br.com.treswxsolutions.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupeBySeller();
+	
+	
+	@Query("SELECT new br.com.treswxsolutions.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals))"
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupeBySeller();
+
 
 }
--- backend/src/main/java/br/com/treswxsolutions/dsvendas/service/SaleService.java
+++ backend/src/main/java/br/com/treswxsolutions/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package br.com.treswxsolutions.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.treswxsolutions.dsvendas.dto.SaleDTO;
+import br.com.treswxsolutions.dsvendas.dto.SaleSuccessDTO;
+import br.com.treswxsolutions.dsvendas.dto.SaleSumDTO;
 import br.com.treswxsolutions.dsvendas.entities.Sale;
 import br.com.treswxsolutions.dsvendas.repositories.SaleRepository;
 import br.com.treswxsolutions.dsvendas.repositories.SellerRepository;
@@ -26,5 +30,17 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupeBySeller(){
+		return repository.amountGroupeBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupeBySeller(){
+		return repository.successGroupeBySeller();
+	}
+	
+	
 
 }
